83bddc37f7effc00b539f850bc640294a22cf125
walker <walkerwilliamsx@gmail.com>
2021-09-15 16:36:39 -0500

    Fixed style

--- react-app/src/components/Main/main.js
+++ react-app/src/components/Main/main.js
@@ -59,35 +59,5 @@ function Main() {
 		</div>
 	);
 }
-// 	const user = useSelector((state) => state.session.user);
-// 	const servers = useSelector((state) => state.session.servers);
-// 	const friends = useSelector((state) => state.session.friends);
-
-// 	const [serverId, setServerId] = useState('');
-// 	const [channelId, setChannelId] = useState('');
-// 	const [online, setOnline] = useState(false);
-
-// 	const [message, setMessage] = useState('');
-// 	const [sender, setSender] = useState(1);
-// 	const [messages, setMessages] = useState([]);
-// 	const [typing, setTyping] = useState(false);
-
-// 	const [members, setMembers] = useState(0);
-// 	const [video, setVideo] = useState(true);
-// 	const [audio, setAudio] = useState(true);
-
-// 	useEffect(() => {
-// 		if (online) return;
-// 		socket.emit('online', user.id);
-// 		setOnline(true);
-// 	}, [online, user.id]);
-
-// 	return (
-// 		<div className="main-container">
-// 			<h1>Main Page</h1>
-// 			<Friends socket={socket} />
-// 		</div>
-// 	);
-// }
 
 export default Main;
